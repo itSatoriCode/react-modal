418b5850dc95736883726fed3db261f2000c2bd2
MrGeet <aldarbatomunkuev@gmail.com>
2021-11-02 15:25:47 +1300

    starter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import Form from './components/Form/Form';
-import GlobalStyle, { Row, Section, Text } from './globalStyles';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
 function App() {
-	return (
-		<Router>
-			<GlobalStyle />
-			<Row gap="2rem" height="80px" justify="center" align="center" background="green">
-				<Link to="signup">
-					<Text size="2rem"> Signup</Text>
-				</Link>
-				<Link to="/">
-					<Text size="2rem"> Home</Text>
-				</Link>
-			</Row>
-			<Switch>
-				<Route path="/" exact component={() => <Section>HOME</Section>} />
-				<Route path="/signup" component={Form} />
-			</Switch>
-		</Router>
-	);
+	return <></>;
 }
 
 export default App;
